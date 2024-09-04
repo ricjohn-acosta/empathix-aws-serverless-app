828a12223fbb1444460e340ab12ff9b0c99f0b8a
Ric <ricjohn.genoguin@gmail.com>
2024-09-04 15:44:01 +1200

    init: start project

---
+++ src/lib/utils.ts
@@ -0,0 +1,29 @@
+import {clsx, type ClassValue} from "clsx"
+import {twMerge} from "tailwind-merge"
+import {statusType} from "@/components/StatusColumns";
+
+export interface StatusToStepMap {
+    [key: statusType]: number;
+}
+
+export interface StepToStatusMap {
+    [key: number]: statusType;
+}
+
+export function cn(...inputs: ClassValue[]) {
+    return twMerge(clsx(inputs))
+}
+
+export const statusToStep: StatusToStepMap = {
+    "sourced": 1,
+    "in-progress": 2,
+    "interview": 3,
+    "hired": 4,
+}
+
+export const stepToStatus: StepToStatusMap = {
+    1: "sourced",
+    2: "in-progress",
+    3: "interview",
+    4: "hired",
+}
